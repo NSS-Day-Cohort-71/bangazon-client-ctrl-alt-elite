72f8cc7faf3ae42c29c6c9597b66d9fe355fe404
Caleb Ford <caleb.ford.sto@gmail.com>
2024-09-25 15:55:50 -0500

    working commit

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import '../global.css'
 export default function Bangazon({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page)
-
+  // console.log(`This is the current Component:`, Component.name)
   return getLayout(
     
       <Component {...pageProps} />
