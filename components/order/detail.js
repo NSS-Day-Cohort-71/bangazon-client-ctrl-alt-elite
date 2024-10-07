9442d986d5775a122169ec0387c3213a7a932ff1
Caleb Ford <caleb.ford.sto@gmail.com>
2024-10-07 14:08:34 -0500

    fix for deleting a single item from cart

--- components/order/detail.js
+++ components/order/detail.js
@@ -7,13 +7,13 @@ export default function CartDetail({ cart, removeProduct }) {
   return (
     <Table headers={headers} footers={footers}>
       {
-        cart.products?.map(product => {
+        cart.lineitems?.map(lineitem => {
           return (
-            <tr key={product.id}>
-              <td>{product.name}</td>
-              <td>{product.price}</td>
+            <tr key={lineitem.id}>
+              <td>{lineitem.product.name}</td>
+              <td>{lineitem.product.price}</td>
               <td>
-                <span className="icon is-clickable" onClick={() => removeProduct(product.id)}>
+                <span className="icon is-clickable" onClick={() => removeProduct(lineitem.id)}>
                   <i className="fas fa-trash"></i>
                 </span>
               </td>
